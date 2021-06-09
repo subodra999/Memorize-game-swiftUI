5be3c8ffb65cce6165b4eb591dbf2b0851607d48
SUBODRA BANIK <subodra999@gmail.com>
2021-06-10 00:18:04 +0530

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Subodra Banik on 10/06/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
