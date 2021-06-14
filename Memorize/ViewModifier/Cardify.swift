65d28ec320ad39c999a688759f6bcd89f454e55b
SUBODRA BANIK <subodra999@gmail.com>
2021-06-14 21:46:02 +0530

    [lecture 7] implicit animation (rotate text on match!)

---
+++ Memorize/ViewModifier/Cardify.swift
@@ -0,0 +1,39 @@
+//
+//  Cardify.swift
+//  Memorize
+//
+//  Created by Subodra Banik on 14/06/21.
+//
+
+import SwiftUI
+
+struct Cardify: ViewModifier {
+    
+    var isFaceUp: Bool
+    
+    func body(content: Content) -> some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.strokeBorder(lineWidth: DrawingConstants.lineWidth)
+            } else {
+                shape.fill()
+            }
+            content
+                .opacity(isFaceUp ? 1 : 0) // for animation to happen
+        }
+    }
+    
+    private struct DrawingConstants {
+        static let cornerRadius: CGFloat = 10
+        static let lineWidth: CGFloat = 3
+    }
+    
+}
+
+extension View {
+    func cardify(isFaceUp: Bool) -> some View {
+        self.modifier(Cardify(isFaceUp: isFaceUp))
+    }
+}
